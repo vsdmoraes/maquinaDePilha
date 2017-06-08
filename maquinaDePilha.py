e312f5679d773f941b7e136a493be6b999d98038
Vinicius Moraes <vsdmoraes@inf.ufpel.edu.br>
2017-06-08 18:02:47 -0300

    fix print - rename add

--- maquinaDePilha.py
+++ maquinaDePilha.py
@@ -19,8 +19,8 @@ def sub():
 	values.append(count)
 	
 try:
-    #files = open(sys.argv[1], "r")
-    files = open("test.txt", "r")
+    files = open(sys.argv[1], "r")
+    #files = open("test.txt", "r")
 except:
     sys.exit("ERROR: Wrong or missing file")
 
@@ -28,26 +28,40 @@ stack = files.read().split()
 values = []
 
 for i in range(0,len(stack)):
-	if(stack[i] == "PUSH"):
+	if(stack[i].upper() == "PUSH"):
 		values.append(float(stack[i+1]))
-	elif (stack[i] == "ADD"):
-		add()
-	elif (stack[i] == "MULT"):
-		mult()
-	elif (stack[i] == "DIV"):
-		div()
-	elif (stack[i] == "SUB"):
-		sub()
-	elif (stack[i] == "PRINT"):
-		save = values.pop()
-		values.append(save)
-		print(save)
+	elif (stack[i].upper() == "SUM"):
+		try:
+			add()
+		except:
+			print("The stack is empty.")
+	elif (stack[i].upper() == "MULT"):
+		try:
+			mult()
+		except:
+			print("The stack is empty.")
+	elif (stack[i].upper() == "DIV"):
+		try:
+			div()
+		except:
+			print("The stack is empty.")
+	elif (stack[i].upper() == "SUB"):
+		try:
+			sub()
+		except:
+			print("The stack is empty.")
+	elif (stack[i].upper() == "PRINT"):
+		print(values.pop())
+		break
 	else:
 		try:
-			if(stack[i-1] == "PUSH"):
+			if(stack[i-1].upper() == "PUSH"):
 				continue
+			else:
+				print("\""+stack[i]+"\" "+"is undefined.")	
+				break
 		except:
-			print(stack[i],"is undefined.")	
+			print("\""+stack[i]+"\""+"is undefined.")	
 			break
 
 files.close()
